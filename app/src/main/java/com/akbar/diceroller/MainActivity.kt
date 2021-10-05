6b3108998c6b1b81412d5dd7f56b012fd12b3411
SMAkbar <sm.akbar42@gmail.com>
2021-10-05 16:10:07 +0500

    completed the roll the dice app

--- app/src/main/java/com/akbar/diceroller/MainActivity.kt
+++ app/src/main/java/com/akbar/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.akbar.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import org.w3c.dom.Text
@@ -35,8 +36,17 @@ class MainActivity : AppCompatActivity() {
         val diceRoll = dice.roll()
 
         // Update the screen with the dice roll
-        val resultTextView : TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+        val diceImage : ImageView = findViewById(R.id.imageView)
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.contentDescription = diceRoll.toString()
+        diceImage.setImageResource(drawableResource)
     }
 }
 
